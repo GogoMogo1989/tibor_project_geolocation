42634d09342e65b3481b32d9219ff1c41caad2cc
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-04-12 18:38:03 +0200

    20230412

--- my_app/src/services/geolocationServices.ts
+++ my_app/src/services/geolocationServices.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -12,11 +12,7 @@ export class GeolocationService {
 
   // Helyzetadatok küldése az adatbázisba
   sendLocation(latitude: number, longitude: number): Observable<any> {
-    const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    };
     const body = { latitude, longitude}; // A helyzetadatokat tartalmazó test
-
-    return this.http.post<any>(this.apiUrl, body, httpOptions); // HTTP POST kérés elküldése
+    return this.http.post<any>(this.apiUrl, body); // HTTP POST kérés elküldése
   }
 }
