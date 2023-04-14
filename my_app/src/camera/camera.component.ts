baee75f62ef83e06f85183cf955ee655be9ec780
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2023-04-14 10:23:54 +0200

    20230414

--- my_app/src/camera/camera.component.ts
+++ my_app/src/camera/camera.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { WebcamImage, WebcamInitError } from 'ngx-webcam';
 import { Subject } from 'rxjs';
 import { WebcamImagesServices } from 'src/services/webcamImagesServices';
@@ -8,11 +8,38 @@ import { WebcamImagesServices } from 'src/services/webcamImagesServices';
   templateUrl: './camera.component.html',
   styleUrls: ['./camera.component.css']
 })
-export class CameraComponent {
+export class CameraComponent implements OnInit {
+
+  public videoOptions: any = {}; // videoOptions objektum a kamera beállításainak tárolására
+  public triggerObservable: Subject<void> = new Subject<void>();
 
   constructor(private webcamImagesServices: WebcamImagesServices){}
 
-  public triggerObservable: Subject<void> = new Subject<void>();
+  ngOnInit(): void {
+    // Rendelkezésre álló médiaeszközök lekérdezése
+    navigator.mediaDevices.enumerateDevices()
+      .then(devices => {
+        // Frontoldali kamera keresése
+        const frontCamera = devices.find(device => device.label.toLowerCase().includes('front'));
+        if (frontCamera) {
+          this.videoOptions.deviceId = { exact: frontCamera.deviceId };
+        } else {
+          console.error('Front camera not found.');
+        }
+      })
+      .catch(error => {
+        console.error('Error enumerating media devices:', error);
+      });
+  }
+
+  // Kamerák közötti váltás
+  public switchCamera(): void {
+    if (this.videoOptions.deviceId.exact === 'front') {
+      this.videoOptions.deviceId.exact = 'rear';
+    } else {
+      this.videoOptions.deviceId.exact = 'front';
+    }
+  }
 
   // Webkamera képének rögzítése
   public webcamImageCapture(): void {
@@ -26,10 +53,10 @@ export class CameraComponent {
 
   // imageCapture esemény kezelése
   public handleImageCapture(event: WebcamImage): void {
-      this.webcamImagesServices.sendImage(event.imageAsDataUrl).subscribe( // Képadatok elküldése az adatbázisba
+    this.webcamImagesServices.sendImage(event.imageAsDataUrl).subscribe(
       (response) => {
         console.log('Kép sikeresen elküldve az adatbázisba!', response);
-        alert('Kép sikeresen mentve!')
+        alert('Kép sikeresen mentve!');
       },
       (error) => {
         console.error('Hiba a kép elküldése során:', error);
